76d01bbf6375bfa57c6e66c945e25ffda6562069
Andy Gómez Ramírez <sammirgomezr@gmail.com>
2025-07-01 17:39:50 -0500

    detallePedido

--- src/modelo/DetallePedido.java
+++ src/modelo/DetallePedido.java
@@ -32,10 +32,79 @@ public class DetallePedido {
         this.tipoPlato = tipoPlato;
         this.estado = estado;
     }
-    
-    
-    
 
-  
+    public int getId_detallepedido() {
+        return id_detallepedido;
+    }
+
+    public void setId_detallepedido(int id_detallepedido) {
+        this.id_detallepedido = id_detallepedido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public void setId_pedido(int id_pedido) {
+        this.id_pedido = id_pedido;
+    }
+
+    public String getTipoPlato() {
+        return tipoPlato;
+    }
+
+    public void setTipoPlato(String tipoPlato) {
+        this.tipoPlato = tipoPlato;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
     
+    
+    
+    
+    
+    
+    
+ 
+    
+    
+ 
 }
